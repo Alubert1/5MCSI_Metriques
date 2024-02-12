baf4a5ac4d47f03121b242741701cc408f24df08
Alubert1 <159786602+Alubert1@users.noreply.github.com>
2024-02-12 18:37:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -36,7 +36,7 @@ def monhistogramme():
     return render_template("histogramme.html")
 
 @app.route("/commits/")
-def monhistogramme():
+def commits():
     return render_template("commit.html")
   
 if __name__ == "__main__":
